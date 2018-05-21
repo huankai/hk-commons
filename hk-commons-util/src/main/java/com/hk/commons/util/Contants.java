5aeb0b1262ead5c6000f9d4d3a79476476b44967
huangk <huankai@139.com>
2018-05-21 18:07:49 +0800

    con

---
+++ hk-commons-util/src/main/java/com/hk/commons/util/Contants.java
@@ -0,0 +1,16 @@
+package com.hk.commons.util;
+
+import java.nio.charset.Charset;
+
+/**
+ * 常量接口
+ *
+ * @author: huangkai
+ * @date 2018-05-18 12:43
+ */
+public interface Contants {
+
+    String UTF_8 = "UTF-8";
+
+    Charset CHARSET_UTF_8 = Charset.forName(UTF_8);
+}
