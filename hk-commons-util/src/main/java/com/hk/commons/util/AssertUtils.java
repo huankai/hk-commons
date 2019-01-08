d8a393337e957126ac6f7f504845a9af70a67d5b
huangk <huankai@139.com>
2019-01-08 17:53:50 +0800

    update

--- hk-commons-util/src/main/java/com/hk/commons/util/AssertUtils.java
+++ hk-commons-util/src/main/java/com/hk/commons/util/AssertUtils.java
@@ -1,5 +1,7 @@
 package com.hk.commons.util;
 
+import java.util.Optional;
+
 /**
  * Assert utils
  *
@@ -14,8 +16,8 @@ public abstract class AssertUtils {
      * @param code   validate String input
      * @param params 国际化消息参数
      */
-    public static void notBlank(String args, Object... params) {
-        notBlankWithDefault(args, "validate.args.notBlank.message", params);
+    public static void notBlankWithI18n(String args, Object... params) {
+        notBlankWithI18n(args, "validate.args.notBlank.message", params);
     }
 
     /**
@@ -25,15 +27,56 @@ public abstract class AssertUtils {
      * @param messageCode Custom validation is not through the error message
      * @param params      国际化消息参数
      */
-    public static void notBlankWithDefault(String code, String messageCode, Object... params) {
+    public static void notBlankWithI18n(String code, String messageCode, Object... params) {
         if (StringUtils.isBlank(code)) {
             throw new IllegalArgumentException(SpringContextHolder.getMessageWithDefault(messageCode, messageCode, params));
         }
     }
 
+    /**
+     * 验证是否为空
+     *
+     * @param args args
+     */
     public static void notEmpty(CharSequence args) {
+        notEmpty(args, "the validate args is null");
+    }
+
+    /**
+     * 验证是否为空
+     *
+     * @param args    args
+     * @param message message
+     */
+    public static void notEmpty(CharSequence args, String message) {
         if (StringUtils.isEmpty(args)) {
-            throw new IllegalArgumentException("null");
+            throw new IllegalArgumentException(message);
+        }
+    }
+
+    /**
+     * object is null
+     *
+     * @param object  object
+     * @param message message
+     */
+    public static void notNull(Object object, String message) {
+        if (object == null || (object instanceof Optional && !((Optional) object).isPresent())) {
+            throw new IllegalArgumentException(message);
+        }
+    }
+
+
+    /**
+     * 检查表达式是否为 true
+     *
+     * @param expression  表达式
+     * @param messageCode 国际化消息Code
+     * @param params      国际化消息参数
+     */
+    public static void isTrue(boolean expression, String message) {
+        if (!expression) {
+            throw new IllegalArgumentException(message);
         }
     }
 
@@ -43,8 +86,8 @@ public abstract class AssertUtils {
      * @param args      args
      * @param paramName 国际化消息参数
      */
-    public static void notEmpty(CharSequence args, Object paramName) {
-        notEmptyWithDefault(args, "validate.args.notEmpty.message", paramName);
+    public static void notEmptyWithI18n(CharSequence args, Object paramName) {
+        notEmptyWithI18n(args, "validate.args.notEmpty.message", paramName);
     }
 
     /**
@@ -54,7 +97,7 @@ public abstract class AssertUtils {
      * @param messageCode 国际化消息Code
      * @param params      国际化消息参数
      */
-    public static void notEmptyWithDefault(CharSequence args, String messageCode, Object... paramName) {
+    public static void notEmptyWithI18n(CharSequence args, String messageCode, Object... paramName) {
         if (StringUtils.isEmpty(args)) {
             throw new IllegalArgumentException(SpringContextHolder.getMessageWithDefault(messageCode, messageCode, paramName));
         }
@@ -67,7 +110,7 @@ public abstract class AssertUtils {
      * @param messageCode 国际化消息Code
      * @param params      国际化消息参数
      */
-    public static void isTrue(boolean expression, String messageCode, Object... params) {
+    public static void isTrueWithI18n(boolean expression, String messageCode, Object... params) {
         if (!expression) {
             throw new IllegalArgumentException(SpringContextHolder.getMessageWithDefault(messageCode, messageCode, params));
         }
